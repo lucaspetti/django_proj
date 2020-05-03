20404ae1eda51c2117ce7b6ffef539545472551c
pettil <lucas.petti@devolute.org>
2020-05-03 19:10:30 +0200

    Include dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
+posts = [
+  {
+    'author': 'Who',
+    'title': '1st Blog Post',
+    'content': '1st Post',
+    'date_posted': 'First May'
+  },
+  {
+    'author': 'Whom',
+    'title': '2nd Blog Post',
+    'content': '2nd Post',
+    'date_posted': 'Second May'
+  }
+]
+
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html')
