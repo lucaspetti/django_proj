6bcf247eb320dc8b7d91eabe3fffeaf77aeae7cc
lucaspetti <luc_petti@hotmail.com>
2020-05-08 18:42:26 +0200

    Include Post model

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-
-posts = [
-  {
-    'author': 'Who',
-    'title': '1st Blog Post',
-    'content': '1st Post',
-    'date_posted': 'First May'
-  },
-  {
-    'author': 'Whom',
-    'title': '2nd Blog Post',
-    'content': '2nd Post',
-    'date_posted': 'Second May'
-  }
-]
+from .models import Post
 
 def home(request):
   context = {
-    'posts': posts,
+    'posts': Post.objects.all(),
     'title': 'All posts'
   }
   return render(request, 'blog/home.html', context)
